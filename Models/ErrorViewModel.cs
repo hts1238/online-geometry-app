d589e7c38cf6077c559dc705dc27204e7bc15622
Timur <hts1238@mail.ru>
2023-08-10 12:52:56 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineGeometryApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
