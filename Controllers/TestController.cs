9b9257c0d353d9bef8eade91d21a5bf2570696f3
Timur <hts1238@mail.ru>
2023-08-12 16:20:10 +0500

    feat: New test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineGeometryApp.Models;
+
+namespace OnlineGeometryApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test(string id, string desc)
+        {
+            TestModel model = new TestModel();
+            model.Id = int.Parse(id);
+            model.Name = "Name";
+            model.Description = desc;
+
+            return View(model);
+        }
+    }
+}
